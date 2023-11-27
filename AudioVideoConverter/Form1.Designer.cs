984eaa355403a8d4f5b8c1c5dfe142e082885da7
kasis <kasis@KASIS>
2023-11-27 18:18:49 +0200

    Add project files.

---
+++ AudioVideoConverter/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace AudioVideoConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            richTextBoxItems = new RichTextBox();
+            buttonAdd = new Button();
+            buttonRemove = new Button();
+            buttonStart = new Button();
+            comboBoxFormats = new ComboBox();
+            label1 = new Label();
+            labelItems = new Label();
+            labelPath = new Label();
+            comboBoxDestination = new ComboBox();
+            label2 = new Label();
+            buttonBrowse = new Button();
+            buttonClear = new Button();
+            progressBarAll = new ProgressBar();
+            SuspendLayout();
+            // 
+            // richTextBoxItems
+            // 
+            richTextBoxItems.Location = new Point(12, 74);
+            richTextBoxItems.Name = "richTextBoxItems";
+            richTextBoxItems.Size = new Size(483, 340);
+            richTextBoxItems.TabIndex = 0;
+            richTextBoxItems.Text = "";
+            // 
+            // buttonAdd
+            // 
+            buttonAdd.Location = new Point(12, 12);
+            buttonAdd.Name = "buttonAdd";
+            buttonAdd.Size = new Size(62, 39);
+            buttonAdd.TabIndex = 1;
+            buttonAdd.Text = "Add";
+            buttonAdd.UseVisualStyleBackColor = true;
+            buttonAdd.Click += buttonAdd_Click;
+            // 
+            // buttonRemove
+            // 
+            buttonRemove.Location = new Point(90, 12);
+            buttonRemove.Name = "buttonRemove";
+            buttonRemove.Size = new Size(62, 39);
+            buttonRemove.TabIndex = 2;
+            buttonRemove.Text = "Remove";
+            buttonRemove.UseVisualStyleBackColor = true;
+            buttonRemove.Click += buttonRemove_Click;
+            // 
+            // buttonStart
+            // 
+            buttonStart.Location = new Point(433, 12);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(62, 39);
+            buttonStart.TabIndex = 3;
+            buttonStart.Text = "Start";
+            buttonStart.UseVisualStyleBackColor = true;
+            buttonStart.Click += buttonStart_Click;
+            // 
+            // comboBoxFormats
+            // 
+            comboBoxFormats.FormattingEnabled = true;
+            comboBoxFormats.Items.AddRange(new object[] { "MP3", "MKV", "AVI", "FLV", "MP4" });
+            comboBoxFormats.Location = new Point(89, 455);
+            comboBoxFormats.Name = "comboBoxFormats";
+            comboBoxFormats.Size = new Size(126, 23);
+            comboBoxFormats.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(15, 458);
+            label1.Name = "label1";
+            label1.Size = new Size(63, 15);
+            label1.TabIndex = 5;
+            label1.Text = "Format To:";
+            // 
+            // labelItems
+            // 
+            labelItems.AutoSize = true;
+            labelItems.Location = new Point(433, 417);
+            labelItems.Name = "labelItems";
+            labelItems.Size = new Size(42, 15);
+            labelItems.TabIndex = 6;
+            labelItems.Text = "Items: ";
+            // 
+            // labelPath
+            // 
+            labelPath.AutoSize = true;
+            labelPath.Location = new Point(12, 552);
+            labelPath.Name = "labelPath";
+            labelPath.Size = new Size(34, 15);
+            labelPath.TabIndex = 7;
+            labelPath.Text = "Path:";
+            // 
+            // comboBoxDestination
+            // 
+            comboBoxDestination.FormattingEnabled = true;
+            comboBoxDestination.Location = new Point(89, 498);
+            comboBoxDestination.Name = "comboBoxDestination";
+            comboBoxDestination.Size = new Size(323, 23);
+            comboBoxDestination.TabIndex = 8;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(15, 501);
+            label2.Name = "label2";
+            label2.Size = new Size(70, 15);
+            label2.TabIndex = 9;
+            label2.Text = "Destination:";
+            // 
+            // buttonBrowse
+            // 
+            buttonBrowse.Location = new Point(418, 497);
+            buttonBrowse.Name = "buttonBrowse";
+            buttonBrowse.Size = new Size(77, 23);
+            buttonBrowse.TabIndex = 10;
+            buttonBrowse.Text = "Browse...";
+            buttonBrowse.UseVisualStyleBackColor = true;
+            buttonBrowse.Click += buttonBrowse_Click;
+            // 
+            // buttonClear
+            // 
+            buttonClear.Location = new Point(174, 12);
+            buttonClear.Name = "buttonClear";
+            buttonClear.Size = new Size(62, 39);
+            buttonClear.TabIndex = 12;
+            buttonClear.Text = "Clear";
+            buttonClear.UseVisualStyleBackColor = true;
+            buttonClear.Click += buttonClear_Click;
+            // 
+            // progressBarAll
+            // 
+            progressBarAll.Location = new Point(12, 422);
+            progressBarAll.Name = "progressBarAll";
+            progressBarAll.Size = new Size(400, 10);
+            progressBarAll.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(552, 576);
+            Controls.Add(progressBarAll);
+            Controls.Add(buttonClear);
+            Controls.Add(buttonBrowse);
+            Controls.Add(label2);
+            Controls.Add(comboBoxDestination);
+            Controls.Add(labelPath);
+            Controls.Add(labelItems);
+            Controls.Add(label1);
+            Controls.Add(comboBoxFormats);
+            Controls.Add(buttonStart);
+            Controls.Add(buttonRemove);
+            Controls.Add(buttonAdd);
+            Controls.Add(richTextBoxItems);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "AuduioVideoConverter";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RichTextBox richTextBoxItems;
+        private Button buttonAdd;
+        private Button buttonRemove;
+        private Button buttonStart;
+        private ComboBox comboBoxFormats;
+        private Label label1;
+        private Label labelItems;
+        private Label labelPath;
+        private ComboBox comboBoxDestination;
+        private Label label2;
+        private Button buttonBrowse;
+        private Button buttonClear;
+        private ProgressBar progressBarAll;
+    }
+}
---
+++ AudioVideoConverter/Form1.cs
@@ -0,0 +1,237 @@
+using FFMpegCore;
+using FFMpegCore.Enums;
+using Microsoft.WindowsAPICodePack.Shell.PropertySystem;
+using Microsoft.WindowsAPICodePack.Shell;
+using System.Diagnostics;
+using Xabe.FFmpeg;
+
+namespace AudioVideoConverter
+{
+    public partial class Form1 : Form
+    {
+        private static string pathToAddFiles = string.Empty;
+        private static string destination = string.Empty;
+        //private static string pathToSaveFiles = string.Empty;
+        private static string[] pathWithFiles = { };
+        private static string[] allFiles = { };
+        private static HashSet<string> filesNames = new HashSet<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            Thread thread = new Thread(PathToDesctop);
+            thread.Start();
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            Clear();
+            GetPath();
+
+            richTextBoxItems.Text = string.Join("\r\n", pathWithFiles);
+
+            AddFilesToHasSet();
+
+            richTextBoxItems.Clear();
+            richTextBoxItems.Text = string.Join("\r\n", filesNames);
+
+            labelPath.Text = $"Path from: {pathToAddFiles}";
+            labelItems.Text = $"Items: {filesNames.Count()}";
+        }
+        private void PathToDesctop()
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            comboBoxDestination.Text = path;
+        }
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            string selectedFile = richTextBoxItems.SelectedText;
+
+            filesNames.Remove(selectedFile);
+
+            richTextBoxItems.Text = string.Join("\r\n", filesNames);
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            Save();
+            comboBoxDestination.Text = destination;
+        }
+        private void Save()
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                destination = folderBrowserDialog1.SelectedPath;
+            }
+        }
+        private string[] GetPath()
+        {
+            OpenFileDialog openFileDialog1 = OpenFile();
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pathToAddFiles = Path.GetDirectoryName(openFileDialog1.FileName)!;
+
+                pathWithFiles = openFileDialog1.FileNames;
+            }
+            return pathWithFiles;
+        }
+
+        private static OpenFileDialog OpenFile()
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog()
+            {
+                InitialDirectory = @"C:\",
+                Title = "Browse AudioVideo Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "All Files",
+                Filter = "All Files (*.*)|*.*",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                Multiselect = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true,
+
+            };
+            return openFileDialog1;//new OpenFileDialog
+
+        }
+
+        private HashSet<string> AddFilesToHasSet()
+        {
+            string[] files = richTextBoxItems.Text.Split("\n");
+            //string[] files = Directory.GetFiles(path);
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                string file = files[i];
+                string[] fileName = file.Split('\\');
+                filesNames.Add(fileName[fileName.Length - 1]);
+            }
+            return filesNames;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            string Mp3 = ".mp3";
+            string Avi = ".avi";
+            string Flv = ".flv";
+            string Mp4 = ".mp4";
+            string Mkv = ".mkv";
+            string otherFormat = comboBoxFormats.Text;
+
+            progressBarAll.Minimum = 0;
+            progressBarAll.Maximum = pathWithFiles.Length;
+            progressBarAll.Value = 0;
+
+            if (comboBoxFormats.Text != "MP3"
+                && comboBoxFormats.Text != "AVI"
+                && comboBoxFormats.Text != "FLV"
+                && comboBoxFormats.Text != "MP4"
+                && comboBoxFormats.Text != "MKV")
+            {
+                Convert(otherFormat);
+            }
+            else if (comboBoxFormats.SelectedItem.ToString() == "MP3")
+            {
+                Convert(Mp3);
+            }
+            else if (comboBoxFormats.SelectedItem.ToString() == "MKV")
+            {
+                Convert(Mkv);
+            }
+            else if (comboBoxFormats.SelectedItem.ToString() == "AVI")
+            {
+                Convert(Avi);
+            }
+            else if (comboBoxFormats.SelectedItem.ToString() == "FLV")
+            {
+                Convert(Flv);
+            }
+            else if (comboBoxFormats.SelectedItem.ToString() == "MP4")
+            {
+                Convert(Mp4);
+            }
+
+            MessageBox.Show("Waiting Complete");
+        }
+
+        private void Convert(string Mp3)
+        {
+            int countFiles = 0;
+
+            foreach (var pathWithFile in pathWithFiles)
+            {
+                countFiles++;
+
+                string[] fileName = pathWithFile.Split('\\');
+                string fileNewFormat = string.Empty;
+                string file = fileName[fileName.Length - 1];
+                string[] arrNewFile = file.Split('.');
+
+                for (int i = 0; i < arrNewFile.Length - 1; i++)
+                {
+                    fileNewFormat += arrNewFile[i];
+                    if (i < arrNewFile.Length - 2)
+                    {
+                        fileNewFormat += '.';
+                    }
+                }
+
+                //For .NotifyOnProgress()
+                //==============================================================================
+                Action<double> progressHandler = new Action<double>(p =>
+                {
+                    //progress value from 0 to 100
+                    Debug.WriteLine(p);
+                });
+
+                ShellObject shell = ShellObject.FromParsingName(pathWithFile);
+                // alternatively: shell.Properties.GetProperty("System.Media.Duration");
+                IShellProperty prop = shell.Properties.System.Media.Duration;
+                // Duration will be formatted as 00:44:08
+                string duration = prop.FormatForDisplay(PropertyDescriptionFormatOptions.None);
+
+                TimeSpan ts = TimeSpan.Parse(duration);
+                //==============================================================================
+
+                fileNewFormat += Mp3;
+                destination = comboBoxDestination.Text;
+
+                string output2 = Path.Combine($"{destination}\\{fileNewFormat}");
+                string inputVideoPath = pathWithFile;
+
+
+                FFMpegArguments
+                    .FromFileInput(inputVideoPath)
+                    .OutputToFile(output2)
+                    //.NotifyOnProgress(progressHandler, ts)
+                    .ProcessSynchronously();
+
+                progressBarAll.Value = countFiles;
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void Clear()
+        {
+            filesNames.Clear();
+            richTextBoxItems.Clear();
+            pathToAddFiles = string.Empty;
+            destination = string.Empty;
+            //pathToSaveFiles = string.Empty;
+            pathWithFiles = null!;
+        }
+    }
+}
